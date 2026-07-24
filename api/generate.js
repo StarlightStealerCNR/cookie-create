2d0632da57e24fa88852d214fb0ffcb08dedb1e5
StarlightStealerCNR <connerl0497@gmail.com>
2026-07-23 21:06:01 -0700

    single line change to api/generate to address functionality discrepencies between formidable v3 and v2

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-const formidable = require("formidable");
+const { formidable } = require("formidable");
 const generateImages = require("../generator");
 const { Ratelimit } = require("@upstash/ratelimit");
 const { Redis } = require("@upstash/redis");
